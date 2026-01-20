f138859e0eb7d9d6fa14911ace4456769c594008
Sneha SB <sneha.sbankar@gmail.com>
2026-01-20 14:18:07 +0530

    Added new config

---
+++ Support/ConfigReader.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+public static class ConfigReader 
+{ 
+    private static IConfigurationRoot _config; 
+    static ConfigReader() 
+    { 
+        _config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory()) 
+            .AddJsonFile("reqnroll.json", optional: false, reloadOnChange: true).Build(); 
+    } 
+    public static string GetBaseUrl() => _config["apiSettings:baseUrl"]; 
+
+}
